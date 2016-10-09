b01965b7c67edbc2279afd614a43dc7b9022291b
Sascha Schneider <foomy.code@arcor.de>
2016-10-09 10:02:44 +0200

    HTML stucture with page.php matched.

--- single.php
+++ single.php
@@ -1,19 +1,22 @@
 <?php get_header(); ?>
-<div id="main">
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<h1 class="c64User"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
-	<div id="meta">
-		erstellt am: <?php the_date('d.m.Y'); ?> |
-		von: <?php the_author(); ?> |
-		Kategorie(n): <?php the_category(', '); ?></p>
-	</div>
-	<article class="entry">
-		<?php the_content(); ?>
-	</article>
-	<?php endwhile; endif; ?>
-</div><!-- main -->
+  <div id="content">
+    <?php if (have_posts()): ?>
+      <?php while (have_posts()): ?>
+        <?php the_post(); ?>
+        <h1 class="c64User"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+        <div id="meta">
+          erstellt am: <?php the_date('d.m.Y'); ?> |
+          von: <?php the_author(); ?> |
+          Kategorie(n): <?php the_category(', '); ?></p>
+        </div>
 
-<div id="sidebar">
-	<?php get_sidebar(); ?>
-</div><!-- sidebar -->
-<?php get_footer(); ?>
+        <article class="entry">
+          <?php the_content(); ?>
+        </article>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div><!-- main -->
+<?php
+  get_sidebar();
+  get_footer();
+?>
