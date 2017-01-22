ceb1fefae16c56ff8eede8cd1a04edd7b93c3f9a
Sascha Schneider <foomy.code@arcor.de>
2017-01-22 08:29:59 +0100

    Changes the headings for the pages.
    Removes the C64 style from the h1 because umlauts are not supported by font.

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
     <?php if (have_posts()): ?>
       <?php while (have_posts()): ?>
         <?php the_post(); ?>
-        <h1 class="c64User"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+        <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
         <div id="meta">
           <?php the_author(); ?> |
           <?php the_date('d.m.Y'); ?>
